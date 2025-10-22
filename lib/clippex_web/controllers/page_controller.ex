f5f3e359a6325f9ebcf82deaa8365e395042d1f2
Bryan Ngo <bryanngo97@gmail.com>
2025-10-22 08:59:46 -0700

    add env var support

--- lib/clippex_web/controllers/page_controller.ex
+++ lib/clippex_web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule ClippexWeb.PageController do
   use ClippexWeb, :controller
 
   def home(conn, _params) do
+    # value = Application.get_env(:clippex, :twitch)[:test]
+    # IO.inspect("Twitch test value: #{value}")
     render(conn, :home)
   end
 end
